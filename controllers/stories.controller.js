f5bb776fdbe8afa13938a58754d9e574d88ffc45
Hanzala Arfat <hanzala735@gmail.com>
2021-04-27 13:18:59 +0530

    add some comment and clean the code

--- controllers/stories.controller.js
+++ controllers/stories.controller.js
@@ -1,9 +1,7 @@
 const Stories = require("../models/stories");
 
-
 exports.createStories = (req, res) => {
-
-    let story_file = [];
+  let story_file = [];
 
   if (req.files.length > 0) {
     story_file = req.files.map((file) => {
@@ -11,11 +9,11 @@ exports.createStories = (req, res) => {
     });
   }
 
-  const story =  new Stories ({
+  const story = new Stories({
     story_file,
     // createdBy: req.user._id,
-    createdBy: req.body.id
-  })
+    createdBy: req.body.id,
+  });
 
   story.save((error, story) => {
     if (error) return res.status(400).json({ error });
@@ -25,41 +23,40 @@ exports.createStories = (req, res) => {
   });
 };
 
-exports.getAllStories = async (req, res) => {
-    const story = await Stories.find() 
-      .exec();
-  
-    res.status(200).json({ story });
-  };
+////// get all story
 
-exports.getviewcount = async(req, res) => {
+exports.getAllStories = async (req, res) => {
+  const story = await Stories.find().exec();
+
+  res.status(200).json({ story });
+};
+
+////////// view count plus after seing//////////////
+
+exports.getviewcount = async (req, res) => {
   const user_id = req.user._id;
   const story_id = req.params.sid;
   // console.log(user_id)
   // console.log(req.user)
-  await Stories.findOne({ _id : story_id }).exec( (error, story) => {
-        if (error) return res.status(400).json({ error });
-        if (story) {
-            console.log(story);
-        }
-  });     
 
-
-  // const story = await Stories.findOneAndUpdate( //{ _id : { $nin: [user_id] }},
-  //   { _id : story_id},
-  //   { $inc: {viewcount:1}, $addToSet:{viewedBy: [user_id]}})  // for addToSet --> the user_id will be inserted in an 
-  //   //array in viewedBy field in mongodb
-  //   .exec()
-  //   .then((result) => {
-  //     res.status(200).json({ result })
-  //   })
-  //   .catch((e) => {
-  //     console.log(e);
-  //     res.status(400).json ( {e} );
-  //   });
-
+  const story = await Stories.findOneAndUpdate(
+    //{ _id : { $nin: [user_id] }},
+    { _id: story_id }, // here surched id if find then below update
+    { $inc: { viewcount: 1 }, $addToSet: { viewedBy: [user_id] } },
+    { new: true }
+  ) // for addToSet --> the user_id will be inserted in an
+    //array in viewedBy field in mongodb
+    .exec()
+    .then((result) => {
+      res.status(200).json({ result });
+    })
+    .catch((e) => {
+      console.log(e);
+      res.status(400).json({ e });
+    });
 };
 
+// below code checking after seeing count 1+ only
 
 // exports.getviewcount = async (req, res) => {
 //   const user_id = req.user._id;
@@ -72,47 +69,42 @@ exports.getviewcount = async(req, res) => {
 //     if (story) {
 //       console.log(story);
 //       // await Stories.findOneAndUpdate({_id: story_id}, {$addToSet:{viewedBy:{viewer: [user_id]}, $inc: {viewedBy:{viewcount:1}}}}).exec()
-      // const viewer_id = req.body.viewedBy.viewer;
-      // console.log(view);
-      // const uid = story.viewedBy.find ( (c) => c.viewer == viewer_id);
-      // console.log(uid);
-      // if (uid){
-      //   Stories.findOne( //{ _id : { $nin: [user_id] }},
-      //     { _id : story_id})
-      //     // { $inc: {viewcount:0}, $addToSet:{viewedBy: [user_id]}})  // for addToSet --> the user_id will be inserted in an 
-      //     //array in viewedBy field in mongodb
-      //     .exec()
-      //     .then((result) => {
-      //       res.status(200).json({ message: "already viewed" })
-      //     })
-      //     .catch((e) => {
-      //       console.log(e);
-      //       res.status(400).json ( {e} );
-      //     });
-      // }
-      // else{
-      //   Stories.findOneAndUpdate( //{ _id : { $nin: [user_id] }},
-      //         { _id : story_id},
-      //         { $inc: {viewcount:1}, $addToSet:{viewedBy: [user_id]}})  // for addToSet --> the user_id will be inserted in an 
-      //         //array in viewedBy field in mongodb
-      //         .exec()
-      //         .then((result) => {
-      //           res.status(200).json({ result })
-      //         })
-      //         .catch((e) => {
-      //           console.log(e);
-      //           res.status(400).json ( {e} );
-      //         });
-      // }
-    // }
-    // console.log(s1);
-    // res.status(200).json({story});
-  // })
+// const viewer_id = req.body.viewedBy.viewer;
+// console.log(view);
+// const uid = story.viewedBy.find ( (c) => c.viewer == viewer_id);
+// console.log(uid);
+// if (uid){
+//   Stories.findOne( //{ _id : { $nin: [user_id] }},
+//     { _id : story_id})
+//     // { $inc: {viewcount:0}, $addToSet:{viewedBy: [user_id]}})  // for addToSet --> the user_id will be inserted in an
+//     //array in viewedBy field in mongodb
+//     .exec()
+//     .then((result) => {
+//       res.status(200).json({ message: "already viewed" })
+//     })
+//     .catch((e) => {
+//       console.log(e);
+//       res.status(400).json ( {e} );
+//     });
+// }
+// else{
+//   Stories.findOneAndUpdate( //{ _id : { $nin: [user_id] }},
+//         { _id : story_id},
+//         { $inc: {viewcount:1}, $addToSet:{viewedBy: [user_id]}})  // for addToSet --> the user_id will be inserted in an
+//         //array in viewedBy field in mongodb
+//         .exec()
+//         .then((result) => {
+//           res.status(200).json({ result })
+//         })
+//         .catch((e) => {
+//           console.log(e);
+//           res.status(400).json ( {e} );
+//         });
+// }
+// }
+// console.log(s1);
+// res.status(200).json({story});
+// })
 
-    
 // };
-// 
-
-
-
-
+//
